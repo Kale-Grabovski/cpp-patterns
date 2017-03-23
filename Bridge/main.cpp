fbf60a4710a1c4319f97a67757c1591ef698a9a0
manny <manny@manny>
2017-03-23 18:30:39 +0300

    Cut some

--- Bridge/main.cpp
+++ Bridge/main.cpp
@@ -4,11 +4,9 @@
 #include "Circle.h"
 
 int main () {
-	std::shared_ptr<DrawApi> rc = std::make_shared<RedCircle>();
-	Circle redCircle(1, 2, 1, rc);
+	Circle redCircle(1, 2, 5, std::make_shared<RedCircle>());
+	Circle greenCircle(6, 8, 9, std::make_shared<GreenCircle>());
+	
 	redCircle.draw();
-
-	std::shared_ptr<DrawApi> gc = std::make_shared<GreenCircle>();
-	Circle greenCircle(6, 8, 9, gc);
 	greenCircle.draw();
 }
