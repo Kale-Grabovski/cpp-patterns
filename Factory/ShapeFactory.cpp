da7106acc74648619a8cfec957d4ba869859d431
manny <manny@manny>
2017-03-27 19:47:24 +0300

    README added

--- Factory/ShapeFactory.cpp
+++ Factory/ShapeFactory.cpp
@@ -4,7 +4,6 @@
 #include "Square.h"
 #include "ShapeFactory.h"
 
-
 std::shared_ptr<Shape> ShapeFactory::getShape(const std::string& shape) const {
     if (shape == "Circle") {
         return std::make_shared<Circle>();
